29770d1e6b22267c004912966be1a800bae738dd
maria_barsallo_lynch@student.hks.harvard.edu <maria_barsallo_lynch@student.hks.harvard.edu>
2019-01-16 14:31:36 -0500

    added import csv code in case wanted to use that option

--- groupveggies.py
+++ groupveggies.py
@@ -1,16 +1,21 @@
 
 #worked with classmate from design school to check a couple of pieces of code
 #read veggies.csv
-from pprint import pprint
+# import csv
+# from pprint import pprint
 
-vegetables = [
- {"name": "eggplant", "color": "purple"},
- {"name": "tomato", "color": "red"},
- {"name": "corn", "color": "yellow"},
- {"name": "okra", "color": "green"},
- {"name": "arugula", "color": "green"},
- {"name": "broccoli", "color": "green"},
-]
+# with open('veggies.csv', 'r') as f:
+#     reader = csv.DictReader(f)
+#     rows = list(reader)
+
+ vegetables = [
+  {"name": "eggplant", "color": "purple"},
+  {"name": "tomato", "color": "red"},
+  {"name": "corn", "color": "yellow"},
+  {"name": "okra", "color": "green"},
+  {"name": "arugula", "color": "green"},
+  {"name": "broccoli", "color": "green"},
+ ]
 #group by color
 
 veggies_by_color = {}
