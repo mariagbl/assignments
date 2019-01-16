8e0f240b9e6bb66d48e41e989007a7c8518d3a56
maria_barsallo_lynch@student.hks.harvard.edu <maria_barsallo_lynch@student.hks.harvard.edu>
2019-01-16 13:16:03 -0500

    edited some of the veggie code for hw

--- veggies.py
+++ veggies.py
@@ -7,3 +7,36 @@ with open('veggies.csv', 'r') as f:
 for row in rows:
     item = dict(row) # Convert Ordered Dict to regular dict (python 3.6 or higher)
     print(item)
+
+
+#Define Headers for CSV
+#Write data-defined vegetables 
+vegetables = [
+  { "name": "eggplant", "color": "purple" },
+  { "name": "tomato", "color": "red" },
+  { "name": "corn", "color": "yellow" },
+  { "name": "okra", "color": "green" },
+  { "name": "arugula", "color": "green" },
+  { "name": "broccoli", "color": "green" }
+]
+
+#Write Headers for CSV
+import csv - #imported csv files
+
+with open('testwrite.csv', 'r') as f: #opened file
+    reader = csv.DictReader(f)
+    rows = list(reader)
+
+#write header
+	writer= csv.writer(f)
+	writerwriterow (['name', 'color'])
+		
+	#Loop through veggies
+	for vegetable in vegetables:
+    item = dict(row) # Convert Ordered Dict to regular dict (python 3.6 or higher)
+    print(item)
+		#Write data 
+		name = vegetable ['name']
+		color = vegetable ['color']
+		row = [name,color]
+		writer.writerow(row)
